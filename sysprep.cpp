e58fbc250c235cc63790dc809f81f3e570bd8537
alikcey <57370184+alikcey@users.noreply.github.com>
2021-04-25 15:53:49 +0300

    Update sysprep.cpp

--- sysprep.cpp
+++ sysprep.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 char   hostname[HOST_NAME_MAX+1];
-char   domainname[HOST_NAME_MAX+1];
 string newhostname;
+string domainsuffix;
 
 void success_handler();
 void error_handler();
@@ -24,6 +24,9 @@ int main(void){
 		}
 	cout << "Current hostname: " << hostname << endl;
 	cout << "Enter new hostname: " << flush; cin >> newhostname;
+	cout << "Enter domain suffix (press ENTER if not needed): " << flush;
+	cin.ignore(numeric_limits <streamsize>::max(), '\n');
+	getline(cin, domainsuffix);
 	if (set_hostname(newhostname) == 0){
 		cout << "New hostname applied successfully." << endl;
 		}
@@ -40,28 +43,17 @@ int main(void){
 void error_handler(){
 	string press_anykey_message = "Press any key to continue...";
 	cout << endl << press_anykey_message << endl;
-	cin.ignore(numeric_limits <streamsize>::max(), '\n');
 	cin.get();
 	}
 
 void success_handler(){
 	string press_anykey_message = "All steps completed successfully. Press any key to continue...";
         cout << endl << press_anykey_message << endl;
-	cin.ignore(numeric_limits <streamsize>::max(), '\n');
 	cin.get();
 	}
 
 
 int set_hostname(string hostname){
-
-        if (getdomainname(domainname, sizeof(domainname)) != 0){
-                cout << "Error while calling getdomainname () function. Error code: " << errno << endl;
-                error_handler();
-                return errno;
-                }
-        cout << "Current domainname: " << hostname << endl;
-	return 0;
-
 
 	fstream hostname_file("/etc/hostname", ofstream::out | ofstream::trunc);
 	fstream hosts_file("/etc/hosts", ofstream::out | ofstream::trunc);
@@ -72,15 +64,27 @@ int set_hostname(string hostname){
 		return errno;
 		}
 
-        hostname_file << hostname << endl;
-        hostname_file.close();
-
 	if (sethostname(hostname.c_str(), hostname.size()) !=0){
                 cout << "Error while calling sethostname () function. Error code: " << errno << endl;
                 error_handler();
                 return errno;
                 }
 
+        hostname_file << hostname << endl;
+        hostname_file.close();
 
+	hosts_file << "127.0.0.1" << '\t' << "localhost" << endl;
+	if (domainsuffix.empty()){
+		hosts_file << "127.0.1.1" << '\t' << newhostname << endl;}
+	else{
+		string FQDN = newhostname + "." + domainsuffix;
+        	hosts_file << "127.0.1.1" << '\t' << FQDN << '\t' << newhostname << endl;
+        	}
+
+	hosts_file.close();
+	return 0;
+	}
+
+int change_machineid(){
 	return 0;
 	}
